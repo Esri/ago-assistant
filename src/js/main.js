d38bfdcfb7bbba10ec3cc37cfa060f8e2dc94eef
Evan Caldwell <evan.caldwell@gmail.com>
2014-11-12 19:32:42 -0500

    handle unsupported types
    Put a generic icon in for anything not found in the types list. This is important for handling new item types that may be created before they've been documented.

--- src/js/main.js
+++ src/js/main.js
@@ -794,6 +794,10 @@ require([
         var info = types.filter(function (item) {
             return item.type === type;
         })[0];
+        if (!info) {
+            // Handle types not found in the above list.
+            return {icon: "datafilesGray"};
+        }
         return info;
     }
 
