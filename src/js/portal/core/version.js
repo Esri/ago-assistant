a4d7e2e57a08b3b36551ee3a93a64731e919746c
Evan Caldwell <evan.caldwell@gmail.com>
2016-09-15 15:12:23 -0400

    missed some es6 upgrades

--- src/js/portal/core/version.js
+++ src/js/portal/core/version.js
@@ -4,12 +4,12 @@ import request from "../request";
  * Return the version of the portal.
  */
 export function version() {
-    var portal = this;
-    var url = portal.portalUrl + "sharing/rest";
-    var parameters = {
+    let portal = this;
+    let url = `${portal.portalUrl}sharing/rest`;
+    let parameters = {
         f: "json"
     };
-    var options = {
+    let options = {
         withCredentials: portal.withCredentials
     };
     return request.get(url, parameters, options);
