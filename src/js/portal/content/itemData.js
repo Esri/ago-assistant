a979145c7fc16fce3784fe5252567a2b42233bf3
Bill Major <bmajor@esri.com>
2017-08-03 12:00:59 -0400

    Fix so that file based items won't display Data section Issue #195

--- src/js/portal/content/itemData.js
+++ src/js/portal/content/itemData.js
@@ -1,6 +1,17 @@
 import request from "../request";
 
-export function itemData(id) {
+export function itemData(id, name) {
+    // If name is passed as an optional parameter, check to see if it
+    //  contains a period.  This is to handle instances where file based
+    //  items aren't displaying JSON properly, trying to download instead.
+    if ((name !== null) && (name !== undefined)) // should handle null and undefined
+    {
+        if (name.indexOf('.') > -1)
+        {
+            console.info("Skipping /data since name is " + name);
+            return Promise.resolve(null);
+        }
+    }
     let portal = this;
     let url = `${portal.portalUrl}sharing/rest/content/items/${id}/data`;
     let parameters = {
